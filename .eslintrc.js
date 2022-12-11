cd246a33ca73a4e622d98cad9cf4211764825ceb
Guany <1084072690@qq.com>
2022-12-11 17:20:28 +0800

    feat: initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
+    commonjs: true,
     es2021: true,
     node: true
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: 'latest'
   },
   rules: {
     'prettier/prettier': 'warn'
