81a7a4b43e7db47323b491e823fa1676b2a95f49
Guany <1084072690@qq.com>
2023-01-31 20:44:10 +0800

    chore: update

--- vue2-vant/src/views/HomeView.vue
+++ vue2-vant/src/views/HomeView.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/images/logo.png" />
-    <div>
-      <van-button type="primary">主要按钮</van-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style lang="less" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
 }
-</script>
+</style>
