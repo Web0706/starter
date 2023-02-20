13a806612681eeedc6a481b0b55c746e7575b7f5
Guany <1084072690@qq.com>
2023-02-20 08:22:13 +0800

    chore: update

---
+++ vite-vue3-ts/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import 'normalize.css'
+import './assets/styles/base.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ vite-vue3/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="main"></main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #ecf0f1;
+}
+</style>
